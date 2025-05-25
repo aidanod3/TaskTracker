c19c065856ab55bd5cb07cdb899b7df940b7124e
aidan <aidanod3@gmail.com>
2025-05-25 16:03:54 -0400

    finished done, todo, and in-progress arguments

--- task-cli.py
+++ task-cli.py
@@ -19,7 +19,7 @@ def save_tasks():
 
 
 def add_task(task_name):
-    tasks.append({"task": task_name, "todo": True, "in-progress": False, "done": False})
+    tasks.append({"task": task_name, "status": "Todo"})
     save_tasks()
     print("task added successfully.")
 
@@ -38,12 +38,59 @@ def delete_task(index):
     print("task deleted successfully")
 
 
-def list_tasks():
+def mark_task_in_progress(index):
+    tasks[int(index) - 1]["status"] = "In Progress"
+    save_tasks()
+    print("task marked successfully.")
+
+
+def mark_task_as_done(index):
+    tasks[int(index) - 1]["status"] = "Done"
+    save_tasks()
+    print("task marked successfully.")
+
+
+def list_all_tasks():
     if len(tasks) == 0:
         print("[no tasks to display.]")
     for x in range(len(tasks)):
-        print(str(x + 1) + ". " + tasks[x]['task'])
+        print(str(x + 1) + ". " + tasks[x]['task'], end=" ")
+        if tasks[x]['status'] == "Todo":
+            print("(Todo)")
+        elif tasks[x]['status'] == "In Progress":
+            print("(In Progress)")
+        else:
+            print("(Done)")
 
+def list_todo_tasks():
+    count = 0
+    for x in range(len(tasks)):
+        if tasks[x]['status'] == "Todo":
+            print(str(x + 1) + ". " + tasks[x]['task'] + " (Todo)")
+            count += 1
+    if count == 0:
+        print("[no tasks to display.]")
+    count = 0
+
+def list_in_progress_tasks():
+    count = 0
+    for x in range(len(tasks)):
+        if tasks[x]['status'] == "In Progress":
+            print(str(x + 1) + ". " + tasks[x]['task'] + " (In Progress)")
+            count += 1
+    if count == 0:
+        print("[no tasks to display.]")
+    count = 0
+
+def list_done_tasks():
+    count = 0
+    for x in range(len(tasks)):
+        if tasks[x]['status'] == "Done":
+            print(str(x + 1) + ". " + tasks[x]['task'] + " (Done)")
+            count += 1
+    if count == 0:
+        print("[no tasks to display.]")
+    count = 0
 
 parser = argparse.ArgumentParser(prog='TaskTracker',
                                  description='keeps track of your tasks!',
@@ -62,12 +109,23 @@ delete_parser = subparser.add_parser("delete", help="Delete task")
 delete_parser.add_argument('index', nargs="?", type=int, help="Optional task index to delete")
 
 mip_parser = subparser.add_parser("mark-in-progress", help="Mark a task as in progress")
+mip_parser.add_argument('index', nargs="?", type=int, help="Optional task index to mark as in progress")
+
 md_parser = subparser.add_parser("mark-done", help="Mark a task as done")
+md_parser.add_argument('index', nargs="?", type=int, help="Optional task index to mark as done")
 
 list_parser = subparser.add_parser("list", help="List all tasks")
+list_subparser = list_parser.add_subparsers(dest='list_command')
+
+list_todo_parser = list_subparser.add_parser('todo', help="List all tasks todo.")
+list_ip_parser = list_subparser.add_parser('in-progress', help="List all tasks in progress.")
+list_done_parser = list_subparser.add_parser('done', help="List all completed tasks.")
+
+
 
 
 args = parser.parse_args()
+
 tasks = load_tasks()
 
 if args.command == "add":
@@ -77,7 +135,7 @@ elif args.command == "update":
         if args.index is not None:
             update_task(args.index)
         else:
-            list_tasks()
+            list_all_tasks()
             print("enter index of task to update: ")
             index = input()
             update_task(index)
@@ -88,11 +146,35 @@ elif args.command == "delete":
         if args.index is not None:
             delete_task(args.index)
         else:
-            list_tasks()
+            list_all_tasks()
             print("enter index of task to delete: ")
             index = input()
             delete_task(index)
     else:
         print("[no tasks to delete.]")
+elif args.command == "mark-in-progress":
+    if args.index is not None:
+        mark_task_in_progress(args.index)
+    else:
+        list_all_tasks()
+        print("enter index of task to mark in progress: ")
+        index = input()
+        mark_task_in_progress(index)
+elif args.command == "mark-done":
+    if args.index is not None:
+        mark_task_as_done(args.index)
+    else:
+        list_all_tasks()
+        print("enter index of task to mark as done: ")
+        index = input()
+        mark_task_as_done(index)
 elif args.command == "list":
-    list_tasks()
+    if args.list_command == "todo":
+        list_todo_tasks()
+    elif args.list_command == "in-progress":
+        list_in_progress_tasks()
+    elif args.list_command == "done":
+        list_done_tasks()
+    else:
+        list_all_tasks()
+
